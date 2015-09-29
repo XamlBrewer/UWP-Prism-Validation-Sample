38edda5ee0cf8215a51b8bf1c11550c2fc3f66af
Diederik Krols <diederik.krols@u2uconsult.com>
2015-09-29 20:04:54 +0200

    Comments in viewmodel.

--- XamlBrewer.Uwp.PrismValidationSample/ViewModels/CivilizationViewModel.cs
+++ XamlBrewer.Uwp.PrismValidationSample/ViewModels/CivilizationViewModel.cs
@@ -1,10 +1,6 @@
 ﻿using Mvvm;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace XamlBrewer.Uwp.PrismValidationSample.ViewModels
@@ -31,7 +27,7 @@ namespace XamlBrewer.Uwp.PrismValidationSample.ViewModels
             set { SetProperty(ref name, value); }
         }
 
-        [RegularExpression(@"[ABCD]\d{2,5}", ErrorMessage="Location is Quadrant (A -> D) and Sector (2 -> 5 digits)")]
+        [RegularExpression(@"[ABCD]\d{2,5}", ErrorMessage = "Location is Quadrant (A -> D) and Sector (2 -> 5 digits)")]
         public string Location
         {
             get { return location; }
@@ -45,17 +41,28 @@ namespace XamlBrewer.Uwp.PrismValidationSample.ViewModels
             set { SetProperty(ref estimatedPopulation, value); }
         }
 
+        // Should have a NotLaterThanProperty attribute.
         public DateTime DiscoveryDate
         {
             get { return discoveryDate; }
-            set { SetProperty(ref discoveryDate, value); }
+            set
+            {
+                SetProperty(ref discoveryDate, value);
+
+                // Doesn't trigger the validation.
+                // this.OnPropertyChanged("MembershipDate"); 
+
+                // Triggers the validation twice. Silly overkill.
+                // MembershipDate = new DateTime(membershipDate.Ticks+1);
+                // MembershipDate = new DateTime(membershipDate.Ticks-1);
+            }
         }
 
-        [LaterThanPropertyAttribute("DiscoveryDate", ErrorMessage="Affiliation date should come after date of first contact.")]
+        [LaterThanPropertyAttribute("DiscoveryDate", ErrorMessage = "Affiliation date should come after date of first contact.")]
         public DateTime MembershipDate
         {
             get { return membershipDate; }
-            set { SetProperty( ref membershipDate, value); }
+            set { SetProperty(ref membershipDate, value); }
         }
 
         public ICommand ValidateCommand
